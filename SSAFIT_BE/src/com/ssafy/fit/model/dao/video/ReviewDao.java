94adb240c87354d2bcd5a5779017be1c76987419
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-03-11 18:49:20 +0900

    update ; ReviewDao

--- SSAFIT_BE/src/com/ssafy/fit/model/dao/video/ReviewDao.java
+++ SSAFIT_BE/src/com/ssafy/fit/model/dao/video/ReviewDao.java
@@ -9,9 +9,9 @@ import com.ssafy.fit.model.dto.video.Review;
 
 public class ReviewDao {
 	private List<Review> list = new ArrayList<>();
+	private List<Review> reviewList;
 	private int no = 0;
 	private static ReviewDao instance;
-	
 	private ReviewDao() {
 		
 	}
@@ -26,12 +26,27 @@ public class ReviewDao {
 	public List<Review> getReviewList() {
 		return list;
 	}
+	
+	// 해당 영상의 리뷰 목록 가져오기
+	public List<Review> getVideoReviewList(int videoNo) {
+		reviewList = new ArrayList<>();
+		for(int i =0; i < list.size(); i++) {
+			Review r = list.get(i);
+			if(r.getVideoNo() == videoNo) {
+				reviewList.add(r);
+			}
+		}
+		return reviewList;
+	}
 
 	// 파라미터로 전송한 리뷰객체를 리스트에 추가하기
 	public void addReview(Review review) {
 		review.setNo(++no);
+		int videoReviewSize = getVideoReviewList(review.getVideoNo()).size()+1;
+		review.setReviewNo(videoReviewSize);
 		review.setDate(LocalDate.now());
 		list.add(review);
 	}
 
 }
+
