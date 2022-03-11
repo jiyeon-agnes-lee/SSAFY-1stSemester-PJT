efc1d67f82d802e25cb26f8f545cc46b466d7b16
jiyeon-agnes-lee <83017334+jiyeon-agnes-lee@users.noreply.github.com>
2022-03-11 18:51:12 +0900

    update : MainServlet

--- SSAFIT_BE/src/com/ssafy/fit/controller/MainServlet.java
+++ SSAFIT_BE/src/com/ssafy/fit/controller/MainServlet.java
@@ -1,7 +1,6 @@
 package com.ssafy.fit.controller;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -40,30 +39,32 @@ public class MainServlet extends HttpServlet {
 			break;
 		}
 	}
+	// 리뷰 등록
 	private void doReviewRegist(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		String title = req.getParameter("title");
 		String content = req.getParameter("content");
 		String userName = req.getParameter("userName");
-
+		int videoNo = Integer.parseInt(req.getParameter("videoNo"));
+		
 		// List에 추가
-		Review review = new Review(title, content, userName);
+		Review review = new Review(videoNo, title, content, userName);
 		ReviewDao.getInstance().addReview(review);
-		
-		int no = review.getNo();
-		int viewCnt = review.getViewCnt();
-		LocalDate date = review.getDate();
-		// 전달 받은 파리미터를 request에 담는다.
-		req.setAttribute("no", no);
-		req.setAttribute("title", title);
-		req.setAttribute("content", content);
-		req.setAttribute("userName", userName);
-		req.setAttribute("viewCnt", viewCnt);
-		req.setAttribute("date", date);
-		
-		doReviewList(req, resp);
+		// 리뷰 등록 시 해당 영상 리뷰 목록 업데이트 후 이동	
+		doReviewList(videoNo, req, resp);
 
 	}
+	// 해당 영상 리뷰
+	private void doReviewList(int videoNo, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ReviewDao dao = ReviewDao.getInstance();
+		List<Review> list = dao.getVideoReviewList(videoNo);
+		request.setAttribute("list", list);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/video/reviewList.jsp");
+		rd.forward(request, response);
+	}
+	// 작성된 모든 리뷰
 	private void doReviewList(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		ReviewDao dao = ReviewDao.getInstance();
 		List<Review> list = dao.getReviewList();
 		request.setAttribute("list", list);
